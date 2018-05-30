58c8233b9ee6dc208795f9bcf91b91e0640f8c03
filipo0on <landedcosine@live.se>
2018-05-30 21:46:23 +0200

    added first login UI and main content container

--- dropboxapp/src/app/login/login.component.ts
+++ dropboxapp/src/app/login/login.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  credentials = {
+    username: '',
+    password: '',
+  };
   constructor() { }
 
   ngOnInit() {
+    console.log(this.credentials.username, 'username');
+    console.log(this.credentials.password, 'password');
+
   }
 
 }
