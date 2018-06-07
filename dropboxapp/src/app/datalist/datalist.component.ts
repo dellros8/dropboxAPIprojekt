a607703b11bfe97042b89e1c150845a7067bbae8
Simon Dellros <simondellros@Simons-MacBook-Air-2.local>
2018-06-07 11:10:15 +0200

    navigation i folders, previous folder, och breadcrumbs fungerar, dock inte clickable breadcrumbs

--- dropboxapp/src/app/datalist/datalist.component.ts
+++ dropboxapp/src/app/datalist/datalist.component.ts
@@ -8,6 +8,9 @@ import { DataService } from '../data.service';
 })
 export class DatalistComponent implements OnInit {
   itemArray = [];
+  pathm = "";
+  breadcrumbs = [];
+
   constructor(private dataservice: DataService) {
     this.dataservice.getFiles();
   }
@@ -16,14 +19,44 @@ export class DatalistComponent implements OnInit {
     this.dataservice.stream
       .subscribe((files) => {
         this.itemArray = files;
-        
+
       });
-   }
-download(path, filetype) {
-    if(filetype === 'file') {
-      this.dataservice.downloadFile(path);
-    }
-}
   }
+  download(path, filetype, filename) {
+    if (filetype === 'file') {
+      this.dataservice.downloadFile(path);
+    } else if (filetype === "folder") {
+
+      this.pathm = path;
+      this.breadcrumbs = path.split("/");
+
+      this.dataservice.dbx.filesListFolder({ path: this.pathm })
+        .then((response) => {
+          this.dataservice.list = response.entries;
+          this.dataservice.stream.next(this.dataservice.list);
+        })
+        .catch((e) => {
+          console.error(e);
+        })
+
+    } else {
+      alert("couldn't download or locate folder");
+    }
+  }
+
+  previousFolder() {
+    const lol = this.pathm.split("/");
+    lol.splice(-1, 1);
+    this.breadcrumbs = lol;
+    const wtf = lol.join("/");
+    this.pathm = wtf;
+  
+    this.dataservice.dbx.filesListFolder({ path: this.pathm })
+        .then((response) => {
+          this.dataservice.list = response.entries;
+          this.dataservice.stream.next(this.dataservice.list);
+        })
+  }
+}
 
 
